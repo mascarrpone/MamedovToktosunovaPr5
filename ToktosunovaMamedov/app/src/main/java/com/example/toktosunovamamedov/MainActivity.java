d00aa2dd80c2bd68b99f26e8a1f45b675eed76df
mascarpone <102468623+mascarrpone@users.noreply.github.com>
2023-03-10 14:30:47 +0700

    Add files via upload

---
+++ ToktosunovaMamedov/app/src/main/java/com/example/toktosunovamamedov/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.toktosunovamamedov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bfffundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
